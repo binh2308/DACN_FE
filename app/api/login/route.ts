a63938c364908b400d5fa1bff3c1ace9347801ce
zuyfarmm <phamduy11112004@gmail.com>
2026-04-05 19:02:40 +0700

    update api endpoint

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
     const { email, password, rememberMe } = await req.json();
 
     //const data = await authLogin({ email, password });
-    const res = await fetch("http://34.30.235.71:3000/auth/login", {
+    const res = await fetch("http://3.26.222.254:3000/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
