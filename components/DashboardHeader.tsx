7f0207f7d195f72d494762686b0428715c690a4f
zuyfarmm <phamduy11112004@gmail.com>
2026-04-24 14:30:14 +0700

    update notifications and dashboard for manager

--- components/DashboardHeader.tsx
+++ components/DashboardHeader.tsx
@@ -227,9 +227,9 @@ export default function DashboardHeader() {
       </div>
 
       <div className="flex items-center gap-4">
-        <button className="relative p-2 hover:bg-neutral-background rounded-lg transition-colors">
+        {/* <button className="relative p-2 hover:bg-neutral-background rounded-lg transition-colors">
           <Mail className="w-5 h-5 text-grey-900" />
-        </button>
+        </button> */}
         <Popover open={notificationsOpen} onOpenChange={handleNotificationsOpenChange}>
           <PopoverTrigger asChild>
             <button
